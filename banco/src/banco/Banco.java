685b6d61d5310e994080e9ed376e3aa2a2b24e8f
Alejandro Quiles <alejandroquilesab@gmail.com>
2019-02-06 15:40:51 +0100

    Modificacion de bancoV2

--- banco/src/banco/Banco.java
+++ banco/src/banco/Banco.java
@@ -15,6 +15,7 @@ public class Banco {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola Banco");
         // TODO code application logic here
     }
     
